4f6948dfe9e197c2af584893796cd78bd7d1fc4a
honeykhra <honeykhra@gmail.com>
2022-03-05 16:58:27 +0530

    basic interface done for form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { Context } from "../context";
+import axios from "axios";
+import { useRouter } from "next/router";
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const { username, setUsername, secret, setSecret } = useContext(Context);
+
+  return (
+    <div className="background">
+      <div className="auth-container">
+        <form
+          action=""
+          className="auth-form"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <div className="auth-title">NextJs Chat</div>
+
+          <div className="input-container">
+            <input
+              placeholder="Email"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="input-container">
+            <input
+              type="password"
+              placeholder="Password"
+              className="text-input"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Login/Sign Up
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 }
